a82a6eae419faf8b957a93a73407ff1de9338990
GlaudistonNeto <glaudistonson@gmail.com>
2021-12-16 19:35:48 -0300

    ending the chat

--- src/server.js
+++ src/server.js
@@ -1,13 +1,31 @@
-const express = require('express');
-const routes = require('./routes');
+const express  = require('express');
+const cors  = require('cors');
+const Pusher = require("pusher");
 
-require('./database');
+const pusher = new Pusher({
+  appId: "1312185",
+  key: "068133b23cfaf634458b",
+  secret: "3f8506aa40993bedbf63",
+  cluster: "us3",
+  useTLS: true
+});
 
 const app = express();
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://localhost:8080']
+}))
+
 app.use(express.json());
 
-app.use(routes);
+app.post('/api/messages', async (req, res) => {
+  await pusher.trigger("chat", "message", {
+    username: req.body.username,
+    message: req.body.message
+  });
 
-app.listen(3333, () => {
-  console.log('Server running on port 3333...');
+  res.json([]);
 });
+
+app.listen(3333);
+console.log('linstening to port 3333');
